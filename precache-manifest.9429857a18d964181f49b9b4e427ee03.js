fb30a54865efaf9e1c3c7a5e30805a37917e702e
Eva Kato <evakato14@gmail.com>
2019-08-14 17:46:34 -0400

    Updates

---
+++ precache-manifest.9429857a18d964181f49b9b4e427ee03.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edec63cd937e1502e6a9a9abd1bfaa32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcb3dda124a4534294d5",
+    "url": "/static/css/main.da344f94.chunk.css"
+  },
+  {
+    "revision": "78245ad06df647ff34d0",
+    "url": "/static/js/2.86f0142f.chunk.js"
+  },
+  {
+    "revision": "bcb3dda124a4534294d5",
+    "url": "/static/js/main.8b6e5478.chunk.js"
+  },
+  {
+    "revision": "b48a976508f873df5ec8",
+    "url": "/static/js/runtime~main.c19bbea5.js"
+  },
+  {
+    "revision": "4452aaf128bb3cb0065af3f4a0425481",
+    "url": "/static/media/02_APompadourTextSample.4452aaf1.ttf"
+  },
+  {
+    "revision": "5b327fa5c067c336edc495c133a20f6d",
+    "url": "/static/media/about.5b327fa5.jpg"
+  },
+  {
+    "revision": "09baf0f8f4e5af5ae795fc2a7ba795f9",
+    "url": "/static/media/cubicle.09baf0f8.png"
+  },
+  {
+    "revision": "9d09411ac2e917ac9c2b90fd3e9d88c0",
+    "url": "/static/media/logo.9d09411a.png"
+  },
+  {
+    "revision": "8b2e41d0f83acdffd5f22bd9282e20ff",
+    "url": "/static/media/yuh.8b2e41d0.jpg"
+  }
+]);
