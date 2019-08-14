b5e9e7ebf4901550ddd02309871ba5251dac219c
Eva Kato <evakato14@gmail.com>
2019-08-14 18:22:58 -0400

    hero fixed

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,28 +1,52 @@
 import React from "react";
+import MediaQuery from "react-responsive";
 import LazyHero from "react-lazy-hero";
 import Background from "../../img/yuh.jpg";
+import MobileBackground from "../../img/mobileHero.jpg";
 import logo from "../../img/logo.png";
 import "./Home.css";
+
+function Text() {
+  return (
+    <div className="main-text">
+      <img src={logo} className="logo" alt="logo" />
+      <h1>Hashtag Cuber</h1>
+      <h3>Eva Kato</h3>
+      <h4>
+        <span>Competitive Speedcuber & Content Creator</span>
+      </h4>
+    </div>
+  );
+}
 
 function Home() {
   return (
     <React.Fragment>
-      <LazyHero
-        imageSrc={Background}
-        minHeight={"100vh"}
-        color={"#4d4747"}
-        opacity={0.3}
-        transitionDuration={400}
-      >
-        <div className="main-text">
-          <img src={logo} className="logo" alt="logo" />
-          <h1>Hashtag Cuber</h1>
-          <h3>Eva Kato</h3>
-          <h4>
-            <span>Competitive Speedcuber & Content Creator</span>
-          </h4>
-        </div>
-      </LazyHero>
+      <MediaQuery minWidth={400}>
+        {matches => {
+          return matches ? (
+            <LazyHero
+              imageSrc={Background}
+              minHeight={"100vh"}
+              color={"#4d4747"}
+              opacity={0.3}
+              transitionDuration={400}
+            >
+              <Text />
+            </LazyHero>
+          ) : (
+            <LazyHero
+              imageSrc={MobileBackground}
+              minHeight={"100vh"}
+              color={"#4d4747"}
+              opacity={0.3}
+              transitionDuration={400}
+            >
+              <Text />
+            </LazyHero>
+          );
+        }}
+      </MediaQuery>
     </React.Fragment>
   );
 }
