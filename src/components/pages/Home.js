0138698642ce1c455bc3c371cf9f98a808186ddc
Eva Kato <evakato14@gmail.com>
2019-08-14 18:32:36 -0400

    window dimensions

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import MediaQuery from "react-responsive";
 import LazyHero from "react-lazy-hero";
 import Background from "../../img/yuh.jpg";
@@ -19,36 +19,57 @@ function Text() {
   );
 }
 
-function Home() {
-  return (
-    <React.Fragment>
-      <MediaQuery minWidth={400}>
-        {matches => {
-          return matches ? (
-            <LazyHero
-              imageSrc={Background}
-              minHeight={"100vh"}
-              color={"#4d4747"}
-              opacity={0.3}
-              transitionDuration={400}
-            >
-              <Text />
-            </LazyHero>
-          ) : (
-            <LazyHero
-              imageSrc={MobileBackground}
-              minHeight={"100vh"}
-              color={"#4d4747"}
-              opacity={0.3}
-              transitionDuration={400}
-            >
-              <Text />
-            </LazyHero>
-          );
-        }}
-      </MediaQuery>
-    </React.Fragment>
-  );
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { width: 0, height: 0 };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <MediaQuery minWidth={400}>
+          {matches => {
+            return matches ? (
+              <LazyHero
+                imageSrc={Background}
+                minHeight={"100vh"}
+                color={"#4d4747"}
+                opacity={0.3}
+                transitionDuration={400}
+              >
+                <Text />
+              </LazyHero>
+            ) : (
+              <LazyHero
+                imageSrc={MobileBackground}
+                minHeight={this.state.height + "px"}
+                color={"#4d4747"}
+                opacity={0.3}
+                transitionDuration={400}
+              >
+                <Text />
+              </LazyHero>
+            );
+          }}
+        </MediaQuery>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
